9fdcf40ba1cafdee33e15318da61bb0d4d427381
podhmo <ababjam61+github@gmail.com>
2017-06-30 22:41:59 +0900

    fix gen implementation

--- bin/gen.py
+++ bin/gen.py
@@ -1,11 +1,16 @@
 # -*- coding:utf-8 -*-
+import itertools
 from collections import OrderedDict
-from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.ext import automap
 from sqlalchemy import create_engine
 from sqlalchemy.inspection import inspect
 from dictknife import loading
 import logging
 logger = logging.getLogger(__name__)
+
+
+def _merge(*xss):
+    return [xs for xs in xss if xs]
 
 
 class Collector:
@@ -23,25 +28,61 @@ class Collector:
         d = OrderedDict()
         for prop in sorted(m.iterate_properties, key=lambda x: x.key):
             if hasattr(prop, "direction"):
-                pairs = prop.synchronize_pairs
-                assert len(pairs) == 1, "multi keys are not supported"
-                d[prop.key] = OrderedDict([
-                    ("table", prop.target.fullname),
-                    ("direction", prop.direction.name),
-                    ("uselist", prop.uselist),
-                    ("relation", OrderedDict([
-                        ("to", "{}.{}".format(pairs[0][0].table.fullname, pairs[0][0].name)),
-                        ("from", "{}.{}".format(pairs[0][1].table.fullname, pairs[0][1].name)),
-                    ])),
-                ])
+                d[prop.key] = OrderedDict(
+                    [
+                        ("table", prop.target.fullname),
+                        ("direction", prop.direction.name),
+                        ("uselist", prop.uselist),
+                        (
+                            "relation", _merge(
+                                self._collect_primary_relation(prop),
+                                self._collect_secondary_relation(prop),
+                            )
+                        ),
+                    ]
+                )
             else:
                 assert len(prop.columns) == 1, "multi keys are not supported"
                 c = prop.columns[0]
-                d[prop.key] = OrderedDict([
-                    ("type", self.resolver.resolve_type(c)),
-                    ("nullable", c.nullable),
-                ])
+                d[prop.key] = OrderedDict(
+                    [
+                        ("type", self.resolver.resolve_type(c)),
+                        ("nullable", c.nullable),
+                    ]
+                )
         return d
+
+    def _collect_primary_relation(self, prop):
+        pairs = prop.synchronize_pairs
+        if not pairs:
+            return []
+        assert len(pairs) == 1, "multi keys are not supported"
+        if prop.parent.local_table == pairs[0][0].table:
+            from_, to = pairs[0][0], pairs[0][1]
+        else:
+            to, from_ = pairs[0][0], pairs[0][1]
+        return OrderedDict(
+            [
+                ("from", "{}.{}".format(from_.table.fullname, from_.name)),
+                ("to", "{}.{}".format(to.table.fullname, to.name)),
+            ]
+        )
+
+    def _collect_secondary_relation(self, prop):
+        pairs = prop.secondary_synchronize_pairs
+        if not pairs:
+            return []
+        assert len(pairs) == 1, "multi keys are not supported"
+        if prop.target == pairs[0][1].table:
+            from_, to = pairs[0][0], pairs[0][1]
+        else:
+            to, from_ = pairs[0][0], pairs[0][1]
+        return OrderedDict(
+            [
+                ("from", "{}.{}".format(from_.table.fullname, from_.name)),
+                ("to", "{}.{}".format(to.table.fullname, to.name)),
+            ]
+        )
 
 
 class Resolver:
@@ -61,10 +102,43 @@ class Resolver:
             return c.type.python_type.__name__  # xxx
 
 
+def guess_name(local_cls, referred_cls, constraint):
+    names = []
+    arrived = set()
+    itr = itertools.chain(
+        [referred_cls.__name__],
+        [
+            col.table.name for col in constraint
+            if col.table not in (local_cls.__table__, referred_cls.__table__)
+        ],
+        [col.name for col in constraint if col.table == local_cls.__table__],
+    )
+    for name in itr:
+        name = name.replace("_id", "")
+        if name.lower() in arrived:
+            continue
+        arrived.add(name)
+        names.append(name)
+    return "_".join(names)
+
+
+def name_for_scalar_relationship(base, local_cls, referred_cls, constraint):
+    return guess_name(local_cls, referred_cls, constraint)
+
+
+def name_for_collection_relationship(base, local_cls, referred_cls, constraint):
+    return guess_name(local_cls, referred_cls, constraint) + "_collection"
+
+
 def main(src):
-    Base = automap_base()
+    Base = automap.automap_base()
     engine = create_engine(src)
-    Base.prepare(engine, reflect=True)
+    Base.prepare(
+        engine,
+        reflect=True,
+        name_for_scalar_relationship=name_for_scalar_relationship,
+        name_for_collection_relationship=name_for_collection_relationship,
+    )
     collector = Collector(Resolver())
     d = collector.collect(sorted(Base.classes, key=lambda x: x.__table__.fullname))
     loading.dumpfile(d, format="json")
